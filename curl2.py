cd555d66e1c7378fc1a04ffd23a050419bd9a4d4
cpi <kiru923@gmail.com>
2017-09-04 09:14:49 +0900

    백업

---
+++ curl2.py
@@ -0,0 +1,75 @@
+#Libraries
+import RPi.GPIO as GPIO
+import time
+import os
+
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 24
+GPIO_ECHO = 23
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+#global
+LED = "LED OFF"
+ 
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+    distance = round(distance, 2)
+ 
+    return distance
+
+def turnLED(dist, condition):
+    dist = distance()
+    global LED
+    if dist <= condition:
+	if LED == "LED OFF":
+		LED = os.popen("curl http://192.168.0.126:9209/led/on").read()
+		print LED
+    else:
+	if LED == "LED ON":
+		LED = os.popen("curl http://192.168.0.126:9209/led/off").read()
+		print LED
+
+
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            turnLED(dist, 20)
+                
+            print "dist : ", dist
+            time.sleep(1)
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print "Measurement stopped by User"
+        GPIO.cleanup()
--- flask_3.py
+++ flask_3.py
@@ -1,6 +1,11 @@
+
 from flask import Flask, request
+import RPi.GPIO as GPIO
 
 app = Flask(__name__) #make instance
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
 
 @app.route('/')
 def index():
@@ -14,5 +19,19 @@ def profile(username):
 def post(post_id):
 	return '<h2>Post ID is %d</h2>' %post_id
 
+@app.route('/led/<led>')
+def led(led):
+	if led == "on":
+		GPIO.output(15, GPIO.HIGH)
+		return "LED ON"
+	elif led == "off":
+		GPIO.output(15, GPIO.LOW)
+		return "LED OFF"
+	else:
+		return "?"
+
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0', port=9209)
+	try:
+		app.run(debug=True, host='0.0.0.0', port=9209)
+	except Keyboardinterrupt:
+		GPIO.cleanup()
