aff784bcfb03cdd637fed6ecb42504bf43d1d5b5
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-04 12:16:42 +0900

    no message

---
+++ dict.py
@@ -0,0 +1,19 @@
+def Articles():
+    articles = [
+        { 'id' : 1,
+          'title' : 'Article One',
+          'body' : 'Hello Dic',
+          'author' : 'chu',
+          'create_date' : '04-09-2017'},
+        {'id': 2,
+         'title': 'Article Two',
+         'body': 'Hello Dic',
+         'author': 'yeon',
+         'create_date': '04-09-2017'},
+        {'id': 3,
+         'title': 'Article Three',
+         'body': 'Hello Dic',
+         'author': 'jin',
+         'create_date': '04-09-2017'}
+    ]
+    return articles
--- flask_9.py
+++ flask_9.py
@@ -1,27 +1,21 @@
 from flask import Flask, render_template
+from dict import Articles
 
 app = Flask(__name__) #make instance
 
-def Articles():
-    articles = [
-        { 'id' : 1,
-          'title' : 'Article One',
-          'body' : 'Hello Dic',
-          'author' : 'chu',
-          'create_date' : '04-09-2017'},
-        {'id': 2,
-         'title': 'Article Two',
-         'body': 'Hello Dic',
-         'author': 'yeon',
-         'create_date': '04-09-2017'},
-        {'id': 3,
-         'title': 'Article Three',
-         'body': 'Hello Dic',
-         'author': 'jin',
-         'create_date': '04-09-2017'}
-    ]
-    return articles
+Articles = Articles()
 
+@app.routo('/')
+def index():
+    return 'hi dictionary'
+
+@app.routo('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+@app.routo('/article/<int:id>')
+def article(id):
+    return render_template('article.html', id=id, articles = Articles)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9209)
