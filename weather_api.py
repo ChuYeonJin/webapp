18995bd7807a2ae450117e9a9ae5c208403a99d4
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 14:56:55 +0900

    no message

--- weather_api.py
+++ weather_api.py
@@ -7,4 +7,13 @@ headers = {"appKey" : "e1e427d8-c745-3632-be37-3416876bf0bb"}
 url = "http://apis.skplanetx.com/weather/current/hourly"
 
 r = requests.get(url, params=params, headers=headers)
-print (r.json())
+
+data = json.loads(r.text)
+weather = data["weather"]["hourly"]
+cTime = weather[0]["timeRelease"]
+cSky = weather[0]["sky"]["name"]
+cWind = weather[0]["wind"]["wspd"]
+cTemp = weather[0]["temperature"]["tc"]
+
+cWeather = "오늘의 날씨 " + cTime + " 기준 하늘은 " + cSky + "이고 풍속은 " + cWind + "이고 기온은 " + cTemp + "입니다."
+print(cWeather)
