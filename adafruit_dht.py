a4912488ba86bcaf33e8b82d311760df0f951cea
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 11:51:44 +0900

    no message

--- adafruit_dht.py
+++ adafruit_dht.py
@@ -5,6 +5,8 @@ pin = 14
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
+print humidity
+
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
