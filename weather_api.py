744a5ea1701bf56d91902aa788377ca0984f1ccc
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 14:31:18 +0900

    no message

--- weather_api.py
+++ weather_api.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 params ={"version" : "1", "city" : "부산", "country" : "수영구", "village" : "광안동"}
-headers = {"appKey" : "e1e427d8-c745-3632-be37-3416876bf0bb","Content-type" : "application/json;charset=UTF-8", "Accept" : "application/json"}
+headers = {"appKey" : "e1e427d8-c745-3632-be37-3416876bf0bb"}
 url = "http://apis.skplanetx.com/weather/current/hourly"
 
 r = requests.get(url, params=params, headers=headers)
