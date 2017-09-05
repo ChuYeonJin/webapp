7a0e61e5b5dfbc51563dff24c2a2e6a5ff39de61
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 14:34:26 +0900

    a

--- weather_api.py
+++ weather_api.py
@@ -2,7 +2,7 @@
 import json
 import requests
 
-params ={"version" : "1", "city" : "부산", "country" : "수영구", "village" : "광안동"}
+params ={"version" : "1", "city" : "부산", "county" : "수영구", "village" : "광안동"}
 headers = {"appKey" : "e1e427d8-c745-3632-be37-3416876bf0bb"}
 url = "http://apis.skplanetx.com/weather/current/hourly"
 
