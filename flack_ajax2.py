972e961e66d821ac014a1cf71496ebce7e8913c4
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 09:37:28 +0900

    no message

---
+++ flack_ajax2.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+import time
+
+app = Flask(__name__)
+
+from sonic import distance
+
+@app.route("/")
+def index():
+    return render_template('sonic.html')
+
+@app.route("/ajax")
+def ajax():
+    result = distance()
+    return jsonify(result=result, time=time)
+
+if __name__ == "__main__":
+	try:
+		 app.run(host='192.168.0.126', port=9209, debug=True)
+
+	except KeyboardInterrupt:
+		GPIO.cleanup()
