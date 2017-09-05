78cda8eb5725e61faaafa23e12f8bfaf0f275d93
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 11:58:45 +0900

    no message

--- adafruit_dht.py
+++ adafruit_dht.py
@@ -1,7 +1,16 @@
 import Adafruit_DHT
+import sys
 
-sensor = Adafruit_DHT.DHT11
-pin = 14
+ensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
+    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
+    sys.exit(1)
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
@@ -11,3 +20,4 @@ if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
+    sys.exit(1)
