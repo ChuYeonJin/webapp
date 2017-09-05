b7f6564e442b9be8e9a598189d668c1cd2339d13
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 14:33:54 +0900

    no message

--- weather_api.py
+++ weather_api.py
@@ -8,19 +8,3 @@ url = "http://apis.skplanetx.com/weather/current/hourly"
 
 r = requests.get(url, params=params, headers=headers)
 print (r.json())
-
-# def weater(appKey, lan, lon):
-#     appKey = "e1e427d8-c745-3632-be37-3416876bf0bb"
-#     headers = {"Content-type" : "application/json;charset=UTF-8", "Accept" : "application/json"}
-#
-#     url = "http://apis.skplanetx.com/weather/current/hourly"
-#
-#     encodingUrl = urllib.urlencode(url)
-#     conn = httplib.HTTPConnection(encodingUrl)
-#
-#     try:
-#         conn.request("GET", headers)
-#         response = conn.getresponse()
-#         print (response)
-#     except:
-#         print ("Connection Failed")
