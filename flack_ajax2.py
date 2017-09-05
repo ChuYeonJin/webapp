4ae3761bc8bffc971e6ff1f65b581de60a315d67
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-05 09:51:30 +0900

    no message

--- flack_ajax2.py
+++ flack_ajax2.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from flask import Flask, request, jsonify, render_template
 import time
 
@@ -12,7 +13,7 @@ def index():
 @app.route("/ajax")
 def ajax():
     result = distance()
-    return jsonify(result=result, curr_time=time.strftime("%Y년 %m월 %d일 / %H : %M"))
+    return jsonify(result=result, curr_time=time.strftime(u"%Y년 %m월 %d일 / %H : %M"))
 
 if __name__ == "__main__":
 	try:
