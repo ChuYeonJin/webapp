801a0999e013f7ffa0928d102b6f8e07759f6cec
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-09-07 11:43:57 +0900

    a

---
+++ mosquitto/mosquitto_test.py
@@ -0,0 +1,25 @@
+#Libraries
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
+
+def on_connect(client, userdata, flags, rc):
+    print("Con : " + str(rc))
+    client.subscribe("test")
+
+def on_message(client, userdata, msg):
+    strMsg = str(msg.payload)
+    print(msg.topic + " : " + strMsg)
+    if(strMsg == "b'LEDON'"):
+        GPIO.output(10, GPIO.HIGH)
+    elif(strMsg == "b'LEDOFF'"):
+        GPIO.output(10, GPIO.LOW)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.0.126", 1883, 60)
+client.loop_forever()
---
+++ pi_server.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+import sonic
+import time
+import httplib, urllib
+
+KEY = "GWD38ZF2B9G2MK0G"
+headers = {"Content-type" : "application/x-www-form-urlencoded", "Accept" : "text/plain"}
+ti = 10
+
+if __name__ == "__main__":
+	try:
+		while True:
+			dist = sonic.distance()
+			if dist > 3000:
+				continue
+
+			params = urllib.urlencode({'field1': dist, 'key': KEY})
+			conn = httplib.HTTPConnection("api.thingspeak.com:80")
+			try:
+				conn.request("POST", "/update", params, headers)
+				response = conn.getresponse()
+				print (response.status, response.reason)
+			except :
+				print ("Connection Failed")
+			time.sleep(ti)
+
+	except KeyboardInterrupt:
+		sonic.GPIO.cleanup()
