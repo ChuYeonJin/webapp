089fdbbe8f544bab025a6fda36bdde49ec545738
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-10 11:45:00 +0900

    a

---
+++ ard/btn_poll.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.IN)
+
+cnt = 0
+
+while True:
+    value = GPIO.input(24)
+    if value == True:
+        count = count + 1
+        print (cnt)
+
+    time.sleep(0.1)
+
