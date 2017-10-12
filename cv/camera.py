5d9f8263bf48043fc55f33b7379ee63d0e313d31
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-12 13:59:03 +0900

    a

--- cv/camera.py
+++ cv/camera.py
@@ -1,10 +1,12 @@
 import cv2
+import os
 
 cam = cv2.VideoCapture(0)
 # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 cam.set(3, 360)
 cam.set(4, 240)
+os.system('sudo modprobe bcm2835-v412')
 
 while True:
     ret,img = cam.read()
