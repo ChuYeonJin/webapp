764536a30fe708695cd8cf8b486f423dfc48df5b
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-12 14:19:35 +0900

    a

--- cv/camera.py
+++ cv/camera.py
@@ -1,11 +1,7 @@
 import cv2
 
 cam = cv2.VideoCapture(0)
-cam2 = cv2.VideoCapture(1)
-cam3 = cv2.VideoCapture(2)
-print(cam)
-print(cam2)
-print(cam3)
+
 # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 cam.set(3, 360)
@@ -16,7 +12,7 @@ while True:
     print(ret)
     print(img)
 
-    cv2.imshow('Video Capture', img)
+    # cv2.imshow('Video Capture', img)
     key = cv2.waitKey(10)
 
     if key == 27:
