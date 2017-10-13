584a43700b2ce1097bb0bb2e4102928111f3c136
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-13 14:23:27 +0900

    a

--- cv/face.py
+++ cv/face.py
@@ -28,13 +28,15 @@ def draw_rects(img, rects, color):
 if __name__ == '__main__':
     cascade_fn = "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
     nested_fn = "/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml"
+    profile_fn = "/usr/local/share/OpenCV/haarcascades/haarcascade_profileface.xml"
 
     cascade = cv2.CascadeClassifier(cascade_fn)
     nested = cv2.CascadeClassifier(nested_fn)
+    profile = cv2.CascadeClassifier(profile_fn)
 
     cam = cv2.VideoCapture(0)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 700)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 700)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
     while True:
         ret, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -43,11 +45,16 @@ if __name__ == '__main__':
         rects = detect(gray, cascade)
         vis = img.copy()
         draw_rects(vis, rects, (0, 255, 0))
+
+        rects2 = detect(gray, profile)
+        draw_rects(vis, rects2, (0, 0, 255))
+
         for x1, y1, x2, y2 in rects:
             roi = gray[y1:y2, x1:x2]
             vis_roi = vis[y1:y2, x1:x2]
             subrects = detect(roi.copy(), nested)
             draw_rects(vis_roi, subrects, (255, 0, 0))
+
         dt = clock() - t
         tup = (20, 20)
         draw_str(vis, tup, 'time: %.1f ms' % (dt * 1000))
